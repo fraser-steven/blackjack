680e7ec10b52c258f7eedd348efd975c7c326959
fraser-steven <92175405+fraser-steven@users.noreply.github.com>
2022-01-26 16:25:15 +0000

    added step 2

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
+import numpy as np 
+import random
+
 def hand_total(hand, dealer_card):
     
     total = sum(hand)
@@ -200,3 +203,132 @@ def BasicStrategy(hand, dealer_card):
     
     if player_total > 16:
         return 'stand'   
+
+
+def hand_result(bet=5, player_hand = 'f', dealer_card = 's', hard_code = 2):
+    
+    deck = np.array([1,2,3,4,5,6,7,8,9,10,10,10,10])
+    
+    hard_stay = False
+    
+    if type(player_hand) == str:
+        
+        player_hand = [random.choice(deck), random.choice(deck)]
+        
+    if player_hand == [1]:
+        hard_stay == True
+    
+    while len(player_hand)<2:
+        
+        player_hand.append(random.choice(deck))
+        
+    if type(dealer_card == str):
+        
+        dealer_card = random.choice(deck)
+    dealer_hand = [dealer_card]
+    
+    if hard_stay == False:
+        
+        if BasicStrategy(player_hand, dealer_hand) == 'BlackJack':
+
+            dealer_hand.append(random.choice(deck))
+            if 1 in (dealer_hand):
+                if sum(dealer_hand)== 11:
+                    return 0
+                #blackjack payed 3/2
+            return bet * 1.5
+
+        if type(hard_code) == str:
+            if hard_code == 'hit':
+                player_hand.append(random.choice(deck))
+                return hand_result(bet, player_hand, dealer_card, hard_code)
+
+            if hard_code == 'double':
+                if len(player_hand) == 2:
+                    player_hand.append(random.choice(deck))
+                    bet = bet * 2
+                    if sum(player_hand) > 21:
+                        return 0 - bet
+                else:
+                    return 'cant double'
+
+            if hard_code == 'split':
+                if len(player_hand) == 2:
+                    if player_hand[0] == player_hand[1]:
+
+                        result = hand_result(bet=bet, player_hand=[player_hand[0]], dealer_card=dealer_card, hard_code= 'Split')
+                        result += hand_result(bet=bet, player_hand=[player_hand[0]], dealer_card=dealer_card, hard_code= 'Split')
+
+                        return result
+
+                    else: 
+                        return BasicStrategy(bet=bet, player_hand= player_hand, dealer_card=dealer_card)
+                else:
+                    return BasicStrategy(bet=bet, player_hand= player_hand, dealer_card=dealer_card)
+        else:
+
+            while BasicStrategy(player_hand, dealer_hand) == 'hit':
+
+                player_hand.append(random.choice(deck))
+                if sum(player_hand)>21:
+                    return 0 - bet
+
+            if BasicStrategy(player_hand, dealer_card) == 'split':
+                result = hand_result(bet=bet, player_hand=[player_hand[0]], dealer_card=dealer_card, hard_code= 'Split')
+                result += hand_result(bet=bet, player_hand=[player_hand[0]], dealer_card=dealer_card, hard_code= 'Split')
+
+                return result
+
+        while True:
+
+            dealer_hand.append(random.choice(deck))
+            dealer_score = sum(dealer_hand)
+            soft_score = dealer_score
+            if dealer_score <= 11 and 1 in dealer_hand:
+                soft_score += 10
+
+            if len(dealer_hand) == 2 and soft_score == 21:
+                return 0 - bet
+
+            player_score = sum(player_hand)
+
+            if player_score <= 11 and 1 in player_hand:
+                player_score += 10
+
+            if soft_score > 21:
+                return bet 
+
+            if player_score > soft_score:
+                return bet
+
+            if player_score == soft_score:
+                return 0
+
+            if player_score < soft_score:
+                return 0 - bet
+    
+    if hard_stay == True:
+        
+        dealer_hand.append(random.choice(deck))
+        dealer_score = sum(dealer_hand)
+        soft_score = dealer_score
+        if dealer_score <= 11 and 1 in dealer_hand:
+            soft_score += 10
+            
+        if len(dealer_score) == 2 and soft_score == 21:
+            return 0 - bet
+        
+        player_score = sum(player_hand)
+        if player_score <= 11 and 1 in player_hand:
+            player_score += 10
+            
+        if soft_score >= 17:
+            if soft_score > 21:
+                return bet 
+            
+            if player_score == soft_score:
+                return 0
+            
+            if player_score < soft_score:
+                return 0 - bet
+            
